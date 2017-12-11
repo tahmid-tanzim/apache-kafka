becec8d78ec302297b933c3ef4dc04e16132ec83
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-12-11 14:03:23 +0600

    HTML Templates

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Home Page")
+    # return HttpResponse("Home Page")
+    return render(request, "home.html")
 
 def about(request):
-    return HttpResponse("About us")
+    # return HttpResponse("About us")
+    return render(request, "about.html")
