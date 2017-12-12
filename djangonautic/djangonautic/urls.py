e97f44cf77670cea1d2d13abe7b6c9c760f6808f
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-12-12 15:06:53 +0600

    Static Files & Images

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.conf.urls import url, include
 from . import views
@@ -24,3 +25,5 @@ urlpatterns = [
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
