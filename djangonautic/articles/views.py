a984f2541b66501eb033e79f8a6f389856e44362
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-12-12 22:23:18 +0600

    URL Parameters

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 def list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/list.html", {"articles": articles})
+
+def details(request, slug):
+    return HttpResponse(slug)
