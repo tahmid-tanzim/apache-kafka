d3d6281e0babd3db3214d56e1cb93c97e4cce423
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-12-14 14:33:06 +0600

    Named URL's

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.list),
-    url(r'^(?P<slug>[\w-]+)/$', views.details),
+    url(r'^$', views.list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.details, name="details"),
 ]
